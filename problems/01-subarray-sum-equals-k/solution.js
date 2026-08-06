d1dab71412b9a212b3954932f6de9b65c522e711
Rue Ellis <rueofherb@gmail.com>
2026-08-06 01:13:01 -0700

    Reset problem 1 solution.js to TODO stub

--- problems/01-subarray-sum-equals-k/solution.js
+++ problems/01-subarray-sum-equals-k/solution.js
@@ -7,25 +7,7 @@
  * @return {number}
  */
 function subarraySum(nums, k) {
-  var subarrays = 0;
-
-  nums.forEach((num, i) => {
-    //Look forward until sum equals k or end of array.
-    var runningTotal = num;
-
-    if (k == num) {
-      subarrays += 1;
-    }
-
-    for (let j = i + 1; j < nums.length; j++) {
-      runningTotal += nums[j];
-
-      if (runningTotal == k) {
-        subarrays++;
-      }
-    }
-  });
-  return subarrays;
+  // TODO: implement
 }
 
 module.exports = { subarraySum };
